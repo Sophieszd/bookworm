7e5efca1b68a6edab7c29d1fd8249ec90a35aa7b
Sophie Szathmary-Dixon <Sophie.Szathmary-Dixon@Nology.local>
2020-02-28 13:59:12 +0000

    added sidebar component

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC4fVmEHVH96X4zrs5a_uVcPiR68vFw7b8",
+    authDomain: "book-worm-cbb06.firebaseapp.com",
+    databaseURL: "https://book-worm-cbb06.firebaseio.com",
+    projectId: "book-worm-cbb06",
+    storageBucket: "book-worm-cbb06.appspot.com",
+    messagingSenderId: "185889340651",
+    appId: "1:185889340651:web:021451c3284f46d4c512ae"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export const firestore = firebase.firestore();
+
+  export default firebase;
