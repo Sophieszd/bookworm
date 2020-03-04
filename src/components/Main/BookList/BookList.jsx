67b2364381466d3c122c1d2aa1b28ccf97da5d53
Sophie Szathmary-Dixon <Sophie.Szathmary-Dixon@Nology.local>
2020-03-04 19:07:28 +0000

    books feeding from firebase

--- src/components/Main/BookList/BookList.jsx
+++ src/components/Main/BookList/BookList.jsx
@@ -1,18 +1,45 @@
 import React, { Component } from "react";
 import styles from "./BookList.module.scss";
 import Book from "./Book";
-import data from "../../../static/data/data";
-// import firebase, { firestore } from "../../firebase";
+// import data from "../../../static/data/data";
+import firebase, { firestore } from "../../../firebase";
 
 export default class BookList extends Component {
-    
+    state = {
+        books: []
+    }
+
+    componentDidMount() {
+        firestore
+            .collection("books")
+            .get()
+            .then((query) => {
+                const books = query.docs.map(doc => doc.data());
+                this.setState({ books });
+                console.log(books)
+            })
+    }
+
     render() {
         return (
-            <section className={styles.BookList}>
-                {data.map((book, index) => (
+            <section className={styles.books}>
+                {this.state.books.map((book, index) => (
                     <Book bookData={book} key={index} />
                 ))}
             </section>
+
         )
     }
 }
+
+// export default class BookList extends Component {
+    
+//     render() {
+//         return (
+//             <section className={styles.BookList}>
+//                 {data.map((book, index) => (
+//                     <Book bookData={book} key={index} />
+//                 ))}
+//             </section>
+//         )
+
